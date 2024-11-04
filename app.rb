712dcbd44f438bf4851c7610746a841cdd3007cc
hanneslindberg <hannes.lindberg@elev.ga.ntig.se>
2024-11-04 15:29:27 +0100

    lägga till

--- app.rb
+++ app.rb
@@ -1,7 +1,30 @@
 class App < Sinatra::Base
 
-    get '/' do
-        erb(:"index")
-    end
+	def db
+		return @db if @db
+
+		@db = SQLite3::Database.new("db/todos.sqlite")
+		@db.results_as_hash = true
+
+		return @db
+	end
+
+	get '/' do
+		erb(:"index")
+	end
+
+	get '/' do
+		@todos = db.execute('SELECT * FROM todos')
+		erb(:"index")
+	end
+
+	post '/todo/add' do 
+
+		name = params["name"]
+		description = params["description"]
+
+		db.execute("INSERT INTO todos (name, description) VALUES(?,?)", [name, description])
+		redirect '/'
+	end
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -3,9 +3,28 @@ require 'sqlite3'
 class Seeder
 
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
   end
 
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS todos')
+  end
+
+  def self.create_tables
+    db.execute('CREATE TABLE todos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                description TEXT)')
+  end
+
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/todos.sqlite')
+    @db.results_as_hash = true
+    @db
+  end
 end
 
 Seeder.seed!
